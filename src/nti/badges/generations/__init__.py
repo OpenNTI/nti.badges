d7efc106a9b666fe4a67e87648f4c5de6233b046
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-15 23:33:31 +0000

    initial badges checkin
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.badges/trunk@39203 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/badges/generations/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+zope.generations support for the dataserver badges.
+
+.. $Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
---
+++ src/nti/badges/views.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+pyramid views.
+
+.. $Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import interface
+from zope.location.interfaces import IContained
+from zope.container import contained as zcontained
+from zope.traversing.interfaces import IPathAdapter
+
+@interface.implementer(IPathAdapter, IContained)
+class NTIBadgesPathAdapter(zcontained.Contained):
+
+    __name__ = 'badges'
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.__parent__ = context
