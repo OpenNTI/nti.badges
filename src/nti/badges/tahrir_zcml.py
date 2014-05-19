265e7464a876193cb26984d7d8ba21c940472264
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-19 21:32:15 +0000

    don't expose autocommit
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.badges/trunk@39353 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/badges/tahrir_zcml.py
+++ src/nti/badges/tahrir_zcml.py
@@ -25,10 +25,9 @@ class IRegisterTahrirDB(interface.Interface):
 	"""
 	name = fields.TextLine(title="db name identifier (site)", required=False, default="")
 	dburi = fields.TextLine(title="db dburi", required=True)
-	autocommit = fields.Bool(title='autocommit flag', required=False, default=True)
 	twophase = fields.Bool(title='two phase commit protocol', required=False, default=False)
 
-def registerTahrirDB(_context, dburi, twophase=False, autocommit=True, name=u""):
+def registerTahrirDB(_context, dburi, twophase=False, autocommit=False, name=u""):
 	"""
 	Register an db
 	"""
