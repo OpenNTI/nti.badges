21a272c47eaa7d22cd6856f9ae2f79cec75b9be4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-20 21:51:12 +0000

    svn is a pain
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.badges/trunk@39474 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/badges/generations/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-zope.generations support for the dataserver badges.
-
-.. $Id$
-"""
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
--- src/nti/badges/utils.py
+++ src/nti/badges/utils.py
@@ -9,9 +9,9 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from . import navstr
-from . import open_interfaces
-
-def tahrir_badge_to_open_badge(badge, issuer_url):
-	result = open_interfaces.IBadgeClass(badge)
-	result.issuer = navstr(issuer_url)
-	return result
+# from . import open_interfaces
+#
+# def tahrir_badge_to_open_badge(badge, issuer_url):
+# 	result = open_interfaces.IBadgeClass(badge)
+# 	result.issuer = navstr(issuer_url)
+# 	return result
