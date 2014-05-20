16b0c9a08eb2c51f83e5750d981b170c9765564f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-20 21:48:28 +0000

    more tahrir to its own pkg
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.badges/trunk@39473 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/badges/tahrir/manager.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+"""
+.. $Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import interface
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from zope.sqlalchemy import ZopeTransactionExtension
+
+from tahrir_api.dbapi import TahrirDatabase
+from tahrir_api.model import DeclarativeBase as tahrir_base
+
+from nti.utils.property import Lazy
+
+from . import interfaces
+
+class NTITahrirDatabase(TahrirDatabase):
+	pass
+
+@interface.implementer(interfaces.ITahrirBadgeManager)
+class TahrirBadgeManager(object):
+
+	def __init__(self, dburi, twophase=False, autocommit=True):
+		self.dburi = dburi
+		self.twophase = twophase
+		self.autocommit = autocommit
+
+	@Lazy
+	def engine(self):
+		result = create_engine(self.dburi)
+		return result
+
+	@Lazy
+	def sessionmaker(self):
+		result = sessionmaker(bind=self.engine,
+							  twophase=self.twophase,
+							  extension=ZopeTransactionExtension())
+		return result
+
+	@Lazy
+	def session(self):
+		result = scoped_session(self.sessionmaker)
+		return result
+
+	@Lazy
+	def db(self):
+		result = NTITahrirDatabase(session=self.session, autocommit=self.autocommit)
+		self.session.configure(bind=self.engine)
+		metadata = getattr(tahrir_base, 'metadata')
+		metadata.create_all(self.engine, checkfirst=True)
+		return result
+
+	def delete_user(self, userid):
+		return self.db.delete_person(userid)
+
+	def get_all_badges(self):
+		result = self.session.get_all_badges()
+		return result
+
+def create_badge_manager(dburi, twophase=False, autocommit=True):
+	result = TahrirBadgeManager(dburi, twophase, autocommit)
+	return result
---
+++ src/nti/badges/tahrir/zcml.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+"""
+Directives to be used in ZCML
+
+$Id$
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import functools
+
+from zope import interface
+from zope.configuration import fields
+from zope.component.zcml import utility
+
+from . import interfaces
+from .manager import create_badge_manager
+
+class IRegisterTahrirDB(interface.Interface):
+	"""
+	The arguments needed for registering an Tahri db
+	"""
+	name = fields.TextLine(title="db name identifier (site)", required=False, default="")
+	dburi = fields.TextLine(title="db dburi", required=True)
+	twophase = fields.Bool(title='two phase commit protocol', required=False, default=False)
+
+def registerTahrirDB(_context, dburi, twophase=False, autocommit=False, name=u""):
+	"""
+	Register an db
+	"""
+	factory = functools.partial(create_badge_manager,
+								dburi=dburi,
+								twophase=twophase,
+								autocommit=autocommit)
+	utility(_context, provides=interfaces.ITahrirBadgeManager,
+			factory=factory, name=name)
