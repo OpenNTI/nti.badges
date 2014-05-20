f8f9be369609412cc581ed4365c7927dbc654145
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-20 05:37:45 +0000

    add adapte tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.badges/trunk@39371 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/badges/tahrir_adapters.py
+++ src/nti/badges/tahrir_adapters.py
@@ -18,21 +18,32 @@ from tahrir_api.model import Person
 from nti.dataserver import interfaces as nti_interfaces
 from nti.dataserver.users import interfaces as user_interfaces
 
+from . import openbadges
 from . import tahrir_interfaces
+from . import interfaces as badge_interfaces
 
 @component.adapter(nti_interfaces.IUser)
 @interface.implementer(tahrir_interfaces.IPerson)
 def user_to_tahrir_person(user):
-    profile = user_interfaces.IUserProfile(user)
-    email = getattr(profile, 'email', None)
-    if not email:
-        raise TypeError("no user email found")
-    result = Person()
-    result.email = email
-    result.nickname = getattr(profile, 'alias', None) or \
-                      getattr(profile, 'realname', None) or \
-                      user.username
-    result.website = getattr(profile, 'home_page', None) or u''
-    result.bio = getattr(profile, 'about', None) or u''
-    result.created_on = datetime.fromtimestamp(user.createdTime)
-    return result
+	profile = user_interfaces.IUserProfile(user)
+	email = getattr(profile, 'email', None)
+	if not email:
+		raise TypeError("no user email found")
+	result = Person()
+	result.email = email
+	result.nickname = getattr(profile, 'alias', None) or \
+					  getattr(profile, 'realname', None) or \
+					  user.username
+	result.website = getattr(profile, 'home_page', None) or u''
+	result.bio = getattr(profile, 'about', None) or u''
+	result.created_on = datetime.fromtimestamp(user.createdTime)
+	return result
+
+@component.adapter(tahrir_interfaces.IPerson)
+@interface.implementer(badge_interfaces.IIdentityObject)
+def person_to_identity_object(person):
+	result = openbadges.IdentityObject(identity=person.email,
+									   type=badge_interfaces.ID_TYPE_EMAIL,
+									   hashed=False,
+									   salt=None)
+	return result
