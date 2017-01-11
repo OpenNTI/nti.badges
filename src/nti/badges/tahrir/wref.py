75cde8d7cdb4767a6e633fe671e3a25107dd35a3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-11 02:57:55 +0000

    don't fail if the ITahrirBadgeManager is not available when getting a weak ref for an assertion
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.badges/trunk@104009 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/badges/tahrir/wref.py
+++ src/nti/badges/tahrir/wref.py
@@ -16,6 +16,8 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope import interface
 
+from zope.interface.interfaces import ComponentLookupError
+
 from nti.badges.tahrir import get_tahrir_assertion_by_id
 
 from nti.badges.tahrir.interfaces import IAssertion
@@ -24,65 +26,70 @@ from nti.property.property import alias
 
 from nti.wref.interfaces import ICachingWeakRef
 
+
 @component.adapter(IAssertion)
 @interface.implementer(ICachingWeakRef)
 class AssertionWeakRef(object):
 
-	_v_assertion = None
+    _v_assertion = None
 
-	uid = alias('_assertion_id')
+    uid = alias('_assertion_id')
 
-	def __init__(self, assertion):
-		self._assertion_id = assertion.id
-		self._assertion_salt = assertion.salt
-		self._assertion_badge_id = assertion.badge_id
-		self._assertion_person_id = assertion.person.email
-		self._v_assertion = assertion
+    def __init__(self, assertion):
+        self._assertion_id = assertion.id
+        self._assertion_salt = assertion.salt
+        self._assertion_badge_id = assertion.badge_id
+        self._assertion_person_id = assertion.person.email
+        self._v_assertion = assertion
 
-	def __getstate__(self):
-		return (1,
-				self._assertion_id,
-				self._assertion_salt,
-				self._assertion_badge_id,
-				self._assertion_person_id)
+    def __getstate__(self):
+        return (1,
+                self._assertion_id,
+                self._assertion_salt,
+                self._assertion_badge_id,
+                self._assertion_person_id)
 
-	def __setstate__(self, state):
-		assert isinstance(state, tuple)
-		assert state[0] == 1
-		self._assertion_id = state[1]
-		self._assertion_salt = state[2]
-		self._assertion_badge_id = state[3]
-		self._assertion_person_id = state[4]
+    def __setstate__(self, state):
+        assert isinstance(state, tuple)
+        assert state[0] == 1
+        self._assertion_id = state[1]
+        self._assertion_salt = state[2]
+        self._assertion_badge_id = state[3]
+        self._assertion_person_id = state[4]
 
-	def __eq__(self, other):
-		try:
-			return other is self or self.__getstate__() == other.__getstate__()
-		except AttributeError:
-			return NotImplemented
+    def __eq__(self, other):
+        try:
+            return other is self or self.__getstate__() == other.__getstate__()
+        except AttributeError:
+            return NotImplemented
 
-	def __hash__(self):
-		return hash(self.__getstate__())
+    def __hash__(self):
+        return hash(self.__getstate__())
 
-	def __call__(self, allow_cached=False):
-		# NOTE: Caching is disabled by default; there may be a ZODB
-		# object cache interaction with SQLalchemy that leads to
-		# ""DetachedInstanceError: Parent instance <Assertion at
-		# 0x49250d50> is not bound to a Session; lazy load operation
-		# of attribute 'badge' cannot proceed"
-		# If so, it's possible this could be solved by the use of "long-lived"
-		# sessions in the ZopeTransactionExtension, such that the session
-		# stays bound to the thread/greenlet...although it may really
-		# need to be bound to the connection, which we could also do with
-		# an approriate session scope
+    def __call__(self, allow_cached=False):
+        # NOTE: Caching is disabled by default; there may be a ZODB
+        # object cache interaction with SQLalchemy that leads to
+        # ""DetachedInstanceError: Parent instance <Assertion at
+        # 0x49250d50> is not bound to a Session; lazy load operation
+        # of attribute 'badge' cannot proceed"
+        # If so, it's possible this could be solved by the use of "long-lived"
+        # sessions in the ZopeTransactionExtension, such that the session
+        # stays bound to the thread/greenlet...although it may really
+        # need to be bound to the connection, which we could also do with
+        # an approriate session scope
 
-		if allow_cached and self._v_assertion is not None:
-			return self._v_assertion
+        if allow_cached and self._v_assertion is not None:
+            return self._v_assertion
 
-		# NOTE: although we are saving the salt of the manager,
-		# let's check all managers. Almost always there is only one
-		# manager registered
+        # NOTE: although we are saving the salt of the manager,
+        # let's check all managers. Almost always there is only one
+        # manager registered
 
-		assertion = get_tahrir_assertion_by_id(self._assertion_id)
-		if assertion is not None:
-			self._v_assertion = assertion
-		return assertion
+        try:
+            assertion = get_tahrir_assertion_by_id(self._assertion_id)
+            if assertion is not None:
+                self._v_assertion = assertion
+            return assertion
+        except ComponentLookupError:  # manager not available
+            logger.error("Tahrir Badge Manager is not available")
+        return None
--- src/nti/badges/tahrir/zcml.py
+++ src/nti/badges/tahrir/zcml.py
@@ -25,52 +25,64 @@ from nti.badges.tahrir.interfaces import ITahrirBadgeManager
 from nti.badges.tahrir.manager import create_issuer
 from nti.badges.tahrir.manager import create_badge_manager
 
+
 class IRegisterTahrirDB(interface.Interface):
-	"""
-	The arguments needed for registering an Tahri db
-	"""
-	dburi = fields.TextLine(title="db dburi", required=False)
-	salt = fields.TextLine(title='assertion salt', required=False)
-	autocommit = fields.Bool(title='autocommit option', required=False, default=False)
-	twophase = fields.Bool(title='two phase commit protocol', required=False, default=False)
-	defaultSQLite = fields.Bool(title='default to SQLLite', required=False, default=False)
-	config = fields.TextLine(title='path to a config file', required=False)
+    """
+    The arguments needed for registering an Tahri db
+    """
+    dburi = fields.TextLine(title="db dburi", required=False)
+    salt = fields.TextLine(title='assertion salt', required=False)
+    autocommit = fields.Bool(title='autocommit option', 
+							 required=False, 
+							 default=False)
+    twophase = fields.Bool(title='two phase commit protocol', 
+						   required=False,
+						   default=False)
+    defaultSQLite = fields.Bool(title='default to SQLLite',
+								required=False, 
+								default=False)
+    config = fields.TextLine(title='path to a config file', required=False)
+
 
 def registerTahrirDB(_context, dburi=None, twophase=False, salt=None,
-					 autocommit=False, defaultSQLite=False, config=None):
-	"""
-	Register an db
-	"""
+                     autocommit=False, defaultSQLite=False, config=None):
+    """
+    Register an db
+    """
 
-	if not dburi and not defaultSQLite and not config:
-		raise ValueError("must specified valid database uri")
+    if not dburi and not defaultSQLite and not config:
+        raise ValueError("must specified valid database uri")
 
-	factory = functools.partial(create_badge_manager,
-								dburi=dburi,
-								salt=salt,
-								twophase=twophase,
-								autocommit=autocommit,
-								defaultSQLite=defaultSQLite,
-								config=config)
-	utility(_context, provides=ITahrirBadgeManager, factory=factory, name="")
+    factory = functools.partial(create_badge_manager,
+                                dburi=dburi,
+                                salt=salt,
+                                twophase=twophase,
+                                autocommit=autocommit,
+                                defaultSQLite=defaultSQLite,
+                                config=config)
+    utility(_context, provides=ITahrirBadgeManager, factory=factory, name="")
+
 
 class IRegisterTahrirIssuer(interface.Interface):
-	"""
-	The arguments needed for registering a Tahri issuer
-	"""
-	name = fields.TextLine(title="Issuer [unique] name")
-	origin = fields.TextLine(title="Issuer origin [URL]")
-	org = fields.TextLine(title="Issuer organization [URL]")
-	contact = fields.TextLine(title="Issuer contact")
-	id = fields.TextLine(title="Issuer zcml identifier", required=False, default='')
+    """
+    The arguments needed for registering a Tahri issuer
+    """
+    name = fields.TextLine(title="Issuer [unique] name")
+    origin = fields.TextLine(title="Issuer origin [URL]")
+    org = fields.TextLine(title="Issuer organization [URL]")
+    contact = fields.TextLine(title="Issuer contact")
+    id = fields.TextLine(title="Issuer zcml identifier", 
+						 required=False,
+						 default='')
+
 
 def registerTahrirIssuer(_context, name, origin, org, contact, id=u''):
-	"""
-	Register a Tahri issuer
-	"""
-	factory = functools.partial(create_issuer,
-								name=name,
-								origin=origin,
-								org=org,
-								contact=contact)
-	utility(_context, provides=IIssuer, factory=factory, name=id)
+    """
+    Register a Tahri issuer
+    """
+    factory = functools.partial(create_issuer,
+                                name=name,
+                                origin=origin,
+                                org=org,
+                                contact=contact)
+    utility(_context, provides=IIssuer, factory=factory, name=id)
