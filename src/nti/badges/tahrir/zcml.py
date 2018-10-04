422764eac1bc74838cd17038964cbfd88d84f719
Josh Zuech <josh.zuech@nextthought.com>
2018-10-04 14:34:00 -0500

    Use zope config Bool

--- src/nti/badges/tahrir/zcml.py
+++ src/nti/badges/tahrir/zcml.py
@@ -16,7 +16,8 @@ from zope import interface
 
 from zope.component.zcml import utility
 
-from zope.schema import Bool
+from zope.configuration.fields import Bool
+
 from zope.schema import TextLine
 
 from nti.badges._compat import text_
